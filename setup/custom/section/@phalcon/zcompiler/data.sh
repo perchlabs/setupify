ab38fc65cd4e8060a96163b1d7c1428233b48b3b
David Schissler <3968727+dschissler@users.noreply.github.com>
2022-04-17 23:29:45 +0300

    Update data.sh

--- setup/custom/section/@phalcon/zcompiler/data.sh
+++ setup/custom/section/@phalcon/zcompiler/data.sh
@@ -1,21 +1,21 @@
 
-ZCOMPILER_DEFAULT_VERSION=0.12.20
+ZCOMPILER_DEFAULT_VERSION=0.16.0
 
 ZCOMPILER_DEFAULT=phar:$ZCOMPILER_DEFAULT_VERSION
 
 # Zephir menu
 MENU_ZCOMPILER_NAME=Zephir
 MENU_ZCOMPILER_GIT_BRANCH_DEFAULT=development
-MENU_ZCOMPILER_GIT_URL_DEFAULT=https://github.com/phalcon/zephir.git
-MENU_ZCOMPILER_BRANCHES=https://github.com/phalcon/zephir/branches
-MENU_ZCOMPILER_VERSIONS=https://github.com/phalcon/zephir/releases
+MENU_ZCOMPILER_GIT_URL_DEFAULT=https://github.com/zephir-lang/zephir.git
+MENU_ZCOMPILER_BRANCHES=https://github.com/zephir-lang/zephir/branches
+MENU_ZCOMPILER_VERSIONS=https://github.com/zephir-lang/zephir/releases
 MENU_ZCOMPILER_TARBALL_EXAMPLES="
   $ZCOMPILER_DEFAULT_VERSION
-  https://github.com/phalcon/zephir/archive/v${ZCOMPILER_DEFAULT_VERSION}.tar.gz
+  https://github.com/zephir-lang/zephir/archive/v${ZCOMPILER_DEFAULT_VERSION}.tar.gz
   file://${HOME}/v${ZCOMPILER_DEFAULT_VERSION}.tar.gz
 "
 MENU_ZCOMPILER_PHAR_EXAMPLES="
   $ZCOMPILER_DEFAULT_VERSION
-  https://github.com/phalcon/zephir/releases/download/${ZCOMPILER_DEFAULT_VERSION}/zephir.phar
+  https://github.com/zephir-lang/zephir/releases/download/${ZCOMPILER_DEFAULT_VERSION}/zephir.phar
   file://${HOME}/zephir.phar
 "
